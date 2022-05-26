fa1e7a74bb0c88cc9ac0bdd8e5f7624ce3e4e599
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-05-26 08:12:11 +0600

    create MyProfileActivity

--- app/src/main/java/com/saikat/emsemployeetracker/LoginActivity.java
+++ app/src/main/java/com/saikat/emsemployeetracker/LoginActivity.java
@@ -2,14 +2,27 @@ package com.saikat.emsemployeetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        btnlogin = findViewById(R.id.btn_login);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MyProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/saikat/emsemployeetracker/MyProfileActivity.java
@@ -0,0 +1,15 @@
+package com.saikat.emsemployeetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+        getSupportActionBar().setTitle("My Profile");
+    }
+}
